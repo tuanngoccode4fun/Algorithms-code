c4d0da393899ada5c3a6ccf0df8d606383527540
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-05-03 22:05:49 +0700

    Recursion_Tower Hanoi

--- Algorithm code/Source.c
+++ Algorithm code/Source.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-int binarySearch(int nums[], int low, int high, int num)
+void TowerOnHanoi(int plates, char robFirst,char robBetween,char robEnd)
 {
-    if (low > high)
+    if (plates == 1)
     {
-        return -1;
+        printf("Plate 1 from %c to %c \n", robFirst, robEnd);
+        return;
     }
-    int midle = low + (high - low) / 2;
-    int value = nums[midle];
+    TowerOnHanoi(plates - 1, robFirst, robEnd, robBetween);
+    printf("Plate %d from %c to %c\n",plates, robFirst, robEnd);
+    TowerOnHanoi(plates - 1, robBetween, robFirst, robEnd);
    
-    if (nums[midle] == num)
-    {
-        printf("value == %d %d %d %d\n", low, high, midle, value);
-        return midle;
-    }
-    if (num < nums[midle])
-    {
-        printf("value < %d %d %d %d\n", low, high, midle, value);
-        return binarySearch(nums, low, midle - 1,num);
-    }
-    else
-    {
-        printf("value > %d %d %d %d\n", low, high, midle, value);
-        return binarySearch(nums, midle+1,high, num);
-    }
-
 }
 void main()
 {
-    int nums[] = { 0,10,20,40,50,60 };
-    int num = 40;
-    int sizenums = sizeof(nums);
-    int sizelow = sizeof(nums[0]);
-    int n = sizeof(nums) / sizeof(nums[0]);
-    printf("Value %ld\n", binarySearch(nums,0,n-1,num));
+    TowerOnHanoi(2, 'A', 'B', 'C');
+   // printf("Value %ld\n");
     
 }
