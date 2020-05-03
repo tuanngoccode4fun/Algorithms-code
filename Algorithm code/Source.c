2e6bef64c6603007f4e50ab90ec6d1319b3f238e
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-05-03 14:34:29 +0700

    Recursion_findBinary

--- Algorithm code/Source.c
+++ Algorithm code/Source.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
- 
-int fibonacci(int n, int a, int b)
+int binarySearch(int nums[], int low, int high, int num)
 {
-    if (n == 0) return a;
-    if (n == 1) return b;
-    return fibonacci(n - 1, b, a + b);
+    if (low > high)
+    {
+        return -1;
+    }
+    int midle = low + (high - low) / 2;
+    int value = nums[midle];
+   
+    if (nums[midle] == num)
+    {
+        printf("value == %d %d %d %d\n", low, high, midle, value);
+        return midle;
+    }
+    if (num < nums[midle])
+    {
+        printf("value < %d %d %d %d\n", low, high, midle, value);
+        return binarySearch(nums, low, midle - 1,num);
+    }
+    else
+    {
+        printf("value > %d %d %d %d\n", low, high, midle, value);
+        return binarySearch(nums, midle+1,high, num);
+    }
+
 }
 void main()
 {
-    printf("Value %ld\n", fibonacci(10,0,1));
+    int nums[] = { 0,10,20,40,50,60 };
+    int num = 40;
+    int sizenums = sizeof(nums);
+    int sizelow = sizeof(nums[0]);
+    int n = sizeof(nums) / sizeof(nums[0]);
+    printf("Value %ld\n", binarySearch(nums,0,n-1,num));
     
 }
