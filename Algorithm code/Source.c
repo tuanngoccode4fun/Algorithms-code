4c046c69d6bc15cedb853a98fe38533b8715da7a
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-05-31 16:46:31 +0700

    git test input number to array

--- Algorithm code/Source.c
+++ Algorithm code/Source.c
@@ -1,28 +1,60 @@
 #include <stdio.h>
-int array[3] = { 3,4,8 };
-int arraytemp[3];
-void makenumber(int n)
+//int array[3] = { 3,4,8 };
+//int arraytemp[3];
+//char c[5] = "abcde";
+//void makenumber(int n)
+//{
+//    for (int i = 0; i < 3; i++)
+//    {
+//        arraytemp[n] = i;
+//        if (n == 2)
+//        {
+//            for (int j = 0; j < 3; j++)
+//            {
+//                printf("%d", array[ arraytemp[j]]);
+//            }
+//            printf("\n");
+//
+//        }
+//        else
+//        {
+//            makenumber(n + 1);
+//        }
+//    }
+//}
+//
+//void main()
+//{
+//    makenumber(0);
+//}
+int array[4] = { 2,4,8,7};
+int remember[5];
+void recursion(int n)
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i <4; i++)
     {
-        arraytemp[n] = i;
+        remember[n] = i;
         if (n == 2)
         {
-            for (int j = 0; j < 3; j++)
+            for (int k = 0; k < 3; k++)
             {
-                printf("%d", array[ arraytemp[j]]);
+                printf("%d", array[remember[k]]);
             }
-            printf("\n");
 
+            printf("\n");
         }
         else
         {
-            makenumber(n + 1);
+            recursion(n + 1);
         }
     }
 }
-
 void main()
 {
-    makenumber(0);
+    for (int k = 0; k < 4; k++)
+    {
+        printf("vui long nhap phan tu thu %d : ", k);
+        scanf_s("%d",&array[k]);
+    }
+    recursion(0);
 }
